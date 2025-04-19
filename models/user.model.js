a818f135b21a0ea8be7b80ec1e6542245c701dfa
trilvid <trilztech34@gmail.com>
2025-04-19 13:30:55 +0100

    updated route

--- models/user.model.js
+++ models/user.model.js
@@ -34,7 +34,7 @@ const user = new mongoose.Schema(
     completed: { type: Boolean, default: false },
     proofs:{type:[],default:[]},
     trader: {type: String},
-    server: { type: String },
+    server: { type: String,default:'server1' },
     rank:{type:String, default:'silver'}
   }
 )
